774f66d98b165aebbecf0f75e492980e0594e420
mati12De <matiasderejeosho@gmail.com>
2024-04-19 19:52:41 +0000

    update video inside

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -14,6 +14,25 @@ const PlayVideo = () => {
     <div className='play-video'>
         <video src={video1} controls autoPlay muted></video>
         <h3>best  Youtube channel to learn web development</h3>
+        <div className='play-video-info'>
+          <p>1525 views &bull; 2days ago</p>
+        <div>
+        <span><img src={like} alt=''/>125</span>
+        <span><img src={dislike} alt=''/>2</span>
+        <span><img src={share } alt=''/>share </span>
+        <span><img src={save} alt=''/>save</span>
+        <span><img src={like}/>125</span>
+       </div>
+    </div>
+    <hr/>
+    <div className="publisher">
+      <img src={jack} alt=''/>
+      <div>
+        <p> GreatStack</p>
+        <span>1m subscribers</span>
+      </div>
+      <button> subscribe</button>
+    </div>
     </div>
   )
 }    
